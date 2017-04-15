49cd0aa9db4a33511b3705fc105ce3b925191b68
mohand reda sakr <mohandsakr69@gmail.com>
2017-04-16 01:13:37 +0200

    Add files via upload

---
+++ Back_to_High_School_Physics.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+//problem link :https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1012
+// Name        : Back_to_High_School_Physics.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//status :accepted
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	 int t,v;
+	 while(cin>>t&&cin>>v){
+		 long result;
+		 result=2*t*v;
+		 cout<<result<<endl;
+
+	 }
+
+
+	return 0;
+}
+
