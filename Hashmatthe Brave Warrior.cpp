c2ea82054ce8548c2e36cf6cbff12e5baf40b09c
mohand reda sakr <mohandsakr69@gmail.com>
2017-04-16 01:33:34 +0200

    Add files via upload

---
+++ Hashmatthe Brave Warrior.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+//problem link:https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=996
+// Name        : Hashmatthe.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//status: accepted
+//============================================================================
+
+
+#include <iostream>
+#include<cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+ long long n,a;
+ while(cin>>n&&cin>>a){
+	 cout<<abs(n-a)<<endl;
+ }
+
+
+
+
+	return 0;
+}
